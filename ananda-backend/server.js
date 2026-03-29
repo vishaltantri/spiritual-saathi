cc54dc3ebb65d45cd2d41a83f58aeed2d03bf4ba
“Vishal <tantrivishal@gmail.com>
2026-03-29 17:23:23 +0530

    fix: update catch-all route to Express 5 syntax

--- ananda-backend/server.js
+++ ananda-backend/server.js
@@ -122,7 +122,7 @@ app.post('/api/chat', async (req, res) => {
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('*', (req, res) => {
+app.get('/{*splat}', (req, res) => {
   res.sendFile(path.join(__dirname, '../ananda-app/dist/index.html'));
 });
 
